628d9f67c97e456278db95934aa267cac70f2d5f
shoya tezono <shoya.tezono@gmail.com>
2026-02-18 13:58:23 +0000

    terraform設定ファイルを修正

--- infra/providers.tf
+++ infra/providers.tf
@@ -1,22 +1,3 @@
-terraform {
-  required_version = ">= 1.6.0"
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "~> 5.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.6"
-    }
-    archive = {
-      source  = "hashicorp/archive"
-      version = "~> 2.4"
-    }
-  }
-
-}
-
 provider "google" {
   project = var.project_id
   region  = var.region
--- infra/versions.tf
+++ infra/versions.tf
@@ -1,13 +1,14 @@
 terraform {
   required_version = ">= 1.5.0"
+
   required_providers {
     google = {
       source  = "hashicorp/google"
       version = "~> 5.0"
     }
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.6"
+    archive = {
+      source  = "hashicorp/archive"
+      version = "~> 2.4"
     }
   }
 }
